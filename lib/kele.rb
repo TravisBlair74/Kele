d93194cc6507119b1172699ee96c5fab2eea2085
Travis-Blair <travisblair74@gmail.com>
2018-02-13 14:03:04 -0500

    Experimenting

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,10 +1,11 @@
+require 'httparty'
+
 class Kele
-  attr_accessor :email, :password, :api_url, :auth_token
+  attr_accessor :api_url, :auth_token
 
   def initialize(email, password)
-    @email, @password = email, password
     @api_url = 'https://www.bloc.io/api/v1'
-    @auth_token = 4
+    @auth_token = HTTParty.get("https://blocapi.docs.apiary.io/#reference/0/sessions/retreive-auth-token", {email: email, password: password})
   end
 
 end
