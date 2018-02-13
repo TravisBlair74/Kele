592034c80d6ff9982a308a5c5bad617a4aec6b42
Travis-Blair <travisblair74@gmail.com>
2018-02-13 15:36:08 -0500

    midway

--- lib/kele.rb
+++ lib/kele.rb
@@ -4,8 +4,9 @@ class Kele
   attr_accessor :api_url, :auth_token
 
   def initialize(email, password)
+
     @api_url = 'https://www.bloc.io/api/v1'
-    @auth_token = HTTParty.get("https://blocapi.docs.apiary.io/#reference/0/sessions/retreive-auth-token", {email: email, password: password})
+    @auth_token = HTTParty.post("https://www.bloc.io/api/v1/sessions", body: {email: email, password: password})
   end
 
 end
