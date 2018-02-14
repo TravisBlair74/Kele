5a6d372db3249a8171fbfb69c890db80603f765a
Travis-Blair <travisblair74@gmail.com>
2018-02-13 19:54:57 -0500

    checkpoint complete

--- lib/kele.rb
+++ lib/kele.rb
@@ -7,7 +7,11 @@ class Kele
   def initialize(email, password)
 
     @api_url = 'https://www.bloc.io/api/v1'
-    @auth_token = HTTParty.post("https://www.bloc.io/api/v1/sessions", body: {email: email, password: password})
+    @auth_token = HTTParty.post(@api_url, body: {email: email, password: password}).parsed_response["auth_token"]
+
+    if @auth_token==nil || @auth_token==false
+      puts "Invalid credentials"
+    end
   end
 
 end
