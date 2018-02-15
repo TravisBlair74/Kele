b3a436b0ca27dce3caf0aace8a7f6d882635097f
Travis-Blair <travisblair74@gmail.com>
2018-02-15 12:59:00 -0500

    First two tasks complete

--- lib/kele.rb
+++ lib/kele.rb
@@ -26,4 +26,14 @@ class Kele
     JSON.parse(response.body)
   end
 
+  def get_roadmap(roadmap_id)
+    response = HTTParty.get("https://www.bloc.io/api/v1/roadmaps/#{roadmap_id}", headers: { "authorization" => @auth_token })
+    JSON.parse(response.body)
+  end
+
+  def get_checkpoint(checkpoint_id)
+    response = HTTParty.get("https://www.bloc.io/api/v1/checkpoints/#{checkpoint_id}", headers: { "authorization" => @auth_token })
+    JSON.parse(response.body)
+  end
+
 end
